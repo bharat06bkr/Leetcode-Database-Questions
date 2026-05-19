dc6a2c36d632fb5eb3d339670276ca3518f13dd7
Bhuma Bharat Kumar Reddy <151626959+bharat06bkr@users.noreply.github.com>
2026-05-19 15:26:14 +0530

    Time: 702 ms (92.84%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -1,11 +1,10 @@
 /* Write your PL/SQL query statement below */
 SELECT p.product_id,
-       ROUND(
-           NVL(SUM(p.price * u.units) / NULLIF(SUM(u.units), 0), 0),
-           2
-       ) AS average_price
-FROM Prices p
-LEFT JOIN UnitsSold u
-ON p.product_id = u.product_id
+    ROUND(
+        NVL(SUM(p.price*u.units)/NULLIF(SUM(u.units),0),0)
+       ,2) 
+    as average_price
+FROM prices p LEFT OUTER JOIN unitssold u
+ON p.product_id=u.product_id
 AND u.purchase_date BETWEEN p.start_date AND p.end_date
 GROUP BY p.product_id;
