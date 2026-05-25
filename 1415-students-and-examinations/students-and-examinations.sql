f2551aa01ca61fbf4e2dd37754ffa451a693ea41
Bhuma Bharat Kumar Reddy <151626959+bharat06bkr@users.noreply.github.com>
2026-05-25 11:29:35 +0530

    Time: 828 ms (87.22%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -0,0 +1,10 @@
+/* Write your PL/SQL query statement below */
+
+SELECT st.student_id,st.student_name,su.subject_name,count(e.student_id) as attended_exams
+FROM  Students st
+CROSS JOIN Subjects su
+LEFT JOIN Examinations e
+    ON st.student_id = e.student_id
+     AND su.subject_name = e.subject_name
+GROUP BY (st.student_id,st.student_name,su.subject_name)
+ORDER BY st.student_id,st.student_name,su.subject_name;
