7820850ac8655f4e35ef81993e8c4abab54c481d
Bhuma Bharat Kumar Reddy <151626959+bharat06bkr@users.noreply.github.com>
2026-05-30 14:59:35 +0530

    Time: 406 ms (35.38%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3848-analyze-subscription-conversion/analyze-subscription-conversion.sql
@@ -0,0 +1,15 @@
+/* Write your PL/SQL query statement below */
+
+SELECT user_id,
+ROUND(AVG(CASE 
+               WHEN activity_type = 'free_trial' 
+               THEN activity_duration 
+           END),2) AS trial_avg_duration,       
+ROUND(AVG(CASE 
+        WHEN activity_type = 'paid' 
+        THEN activity_duration 
+    END),2) AS paid_avg_duration
+FROM UserActivity
+WHERE user_id IN (SELECT user_id FROM UserActivity where activity_type='paid')
+GROUP BY user_id
+ORDER BY 1;
