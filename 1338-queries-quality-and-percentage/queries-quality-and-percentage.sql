958c1813805ef8c67d0516e0e7ba75cecc328721
Bhuma Bharat Kumar Reddy <151626959+bharat06bkr@users.noreply.github.com>
2026-06-03 07:46:02 +0530

    Time: 502 ms (18.26%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -0,0 +1,7 @@
+/* Write your PL/SQL query statement below */
+
+SELECT query_name,
+ROUND(SUM(rating/position)/COUNT(result),2) as quality,
+ROUND(sum(case when rating < 3 then 1 else 0 end)*100/ count(*),2) as poor_query_percentage
+FROM Queries
+GROUP BY query_name;
