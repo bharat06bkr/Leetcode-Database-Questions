83e92b452c6dcd645817fbe2c1c4b143a9a3cded
Bhuma Bharat Kumar Reddy <151626959+bharat06bkr@users.noreply.github.com>
2026-06-03 07:54:40 +0530

    Time: 751 ms (76.64%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1541-top-travellers/top-travellers.sql
@@ -0,0 +1,8 @@
+/* Write your PL/SQL query statement below */
+
+SELECT u.name,
+    NVL(SUM(r.distance),0) as travelled_distance
+FROM Users u LEFT JOIN Rides r
+ON u.id=r.user_id 
+GROUP BY u.id,u.name
+ORDER BY travelled_distance DESC;
