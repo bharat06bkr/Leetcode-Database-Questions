d1d4e65894d1e3df6280f474f3c095ab9ce4ea3c
Bhuma Bharat Kumar Reddy <151626959+bharat06bkr@users.noreply.github.com>
2026-06-03 08:00:18 +0530

    Time: 600 ms (60.95%) | Memory: 0B (100.00%) - LeetSync

--- 2024-calculate-special-bonus/calculate-special-bonus.sql
+++ 2024-calculate-special-bonus/calculate-special-bonus.sql
@@ -1,5 +1,4 @@
-/* Write your PL/SQL query statement below */
-
+# Write your MySQL query statement below
 SELECT employee_id,
     CASE 
         WHEN MOD(employee_id,2)=1 AND name NOT LIKE 'M%' THEN 
